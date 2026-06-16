a3bfeb2adba7f3a859b80aa6ece522fbf1a85ef7
OliverIsaacA <oavalosparedes@gmail.com>
2026-06-16 10:27:34 -0600

    Funciones completas de LibroDAO

--- dao/libro_dao.py
+++ dao/libro_dao.py
@@ -21,3 +21,50 @@ class LibroDAO:
         cursor.close()
         conexion.close()
         return libros
+
+    def insertar(self, libro):
+        conexion = Conexion.obtener_conexion()
+        cursor = conexion.cursor()
+        sql ="""
+        INSERT INTO libro (titulo, autor, isbn, disponible)
+        VALUES (%s, %s, %s, %s)
+        """
+
+        cursor.execute(
+            sql,
+            (libro.titulo, libro.autor, libro.isbn, libro.disponible)
+        )
+
+        conexion.commit()
+        cursor.close()
+        conexion.close()
+
+    def actualizar(self, libro):
+        conexion = Conexion.obtener_conexion()
+        cursor = conexion.cursor()
+
+        sql ="""
+        UPDATE libro
+        SET titulo = %s, autor = %s, isbn = %s, disponible = %s
+        WHERE id = %s
+        """
+
+        cursor.execute(
+            sql,
+            (libro.titulo, libro.autor, libro.isbn, libro.disponible, libro.id)
+        )
+        conexion.commit()
+        cursor.close()
+        conexion.close()
+
+    def eliminar(self, libro_id):
+        conexion = Conexion.obtener_conexion()
+        cursor = conexion.cursor()
+
+        cursor.execute(
+            "DELETE FROM libro WHERE id = %s",
+            (libro_id,)
+            )
+        conexion.commit()
+        cursor.close()
+        conexion.close()
